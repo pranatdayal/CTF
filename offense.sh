105d23b42750ca516449104c05f689dc3ed98c96
Pranat Dayal <pranatdayal@Pranats-MacBook-Pro.local>
2017-10-20 18:38:36 -0400

    updated

--- offense.sh
+++ offense.sh
@@ -4,27 +4,27 @@
 if ! [ -x "$(command -v git)" ]
 then
     echo "[*]Installing GIT"
-    echo "  $(apt-get install git)"
+    apt-get install git
     echo "[+]Done!"
 else
     echo "[+] GIT already installed"
-    echo $(git --version)
+    git --version
 fi
 # download linuxprivchecker
 echo "[*]Downloading Linux priv checker"
-echo "  $(git clone https://github.com/sleventyeleven/linuxprivchecker.git)"
+git clone https://github.com/sleventyeleven/linuxprivchecker.git
 echo "[+]Done!"
 # download python-pty-shells
 echo "[*]Downloading python pty shells"
-echo "  $(git clone https://github.com/pranatdayal/python-pty-shells)"
+git clone https://github.com/pranatdayal/python-pty-shells
 echo "[+]Done!"
 
 # download backdoor-factory
 echo "[*]Downloading backdoor-factory"
-echo "  $(git clone  https://github.com/secretsquirrel/the-backdoor-factory.git)"
+git clone  https://github.com/secretsquirrel/the-backdoor-factory.git
 echo "[+]Done!"
 
 # download routersploit
 echo "[*]Downloading routersploit"
-echo "  $(git clone http://github.com/reverse-shell/routersploit)"
-echo "[+]Done!"
+git clone http://github.com/reverse-shell/routersploit
+echo "[+]Done"
