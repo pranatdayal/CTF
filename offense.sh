7d8c85a6827fae7e7b3d678a96247d43b9948b8c
Pranat Dayal <pranatdayal@Pranats-MacBook-Pro.local>
2017-10-20 20:07:58 -0400

    fixed offense.sh

--- offense.sh
+++ offense.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
+red='\033[0;31m'
+green='\033[0:32m'
+nc='\033[0m'
+
+echo -e "${red}Setting up offense${nc}"
 
 # download and install git
 if ! [ -x "$(command -v git)" ]
 then
-    echo "[*]Installing GIT"
+    echo -e "${red}[*]Installing GIT${nc}"
     apt-get install git
-    echo "[+]Done!"
+    echo -e "${green}[+]Done!${nc}"
 else
-    echo "[+] GIT already installed"
+    echo -e "${green}[+] GIT already installed${nc}"
     git --version
 fi
 # download linuxprivchecker
-echo "[*]Downloading Linux priv checker"
+echo -e "${red}[*]Downloading Linux priv checker${nc}"
 git clone https://github.com/sleventyeleven/linuxprivchecker.git
-echo "[+]Done!"
+echo -e "${green}[+]Done!${nc}"
 # download python-pty-shells
-echo "[*]Downloading python pty shells"
+echo -e "${red}[*]Downloading python pty shells${nc}"
 git clone https://github.com/pranatdayal/python-pty-shells
-echo "[+]Done!"
+echo -e "${green}[+]Done!${nc}"
 
 # download backdoor-factory
-echo "[*]Downloading backdoor-factory"
+echo -e "${red}[*]Downloading backdoor-factory${nc}"
 git clone  https://github.com/secretsquirrel/the-backdoor-factory.git
-echo "[+]Done!"
+echo -e "${green}[+]Done!${nc}"
 
 # download routersploit
-echo "[*]Downloading routersploit"
+echo -e "${red}[*]Downloading routersploit${nc}"
 git clone http://github.com/reverse-shell/routersploit
-echo "[+]Done"
+echo -e "${green}[+]Done!${nc}"
+
+echo " "
+echo -e "${green}[+] Offense setup complete"
